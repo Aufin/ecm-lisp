410a2c7f47983e0509cd2348a3e008982acc1448
drewc - Aufin Shatranj Trading Inc. <me@drewc.ca>
2025-11-06 16:14:30 -0800

    Add syndicate to interim amount report

--- old-maxclaims/maxclaims/src/report/interim.lisp
+++ old-maxclaims/maxclaims/src/report/interim.lisp
@@ -19,22 +19,26 @@ WITH interim AS (
 
     SELECT claim.claim_id, 
            max(COALESCE (ti.date, '1900-01-01')) AS interim_date
-          , adjuster_id, claim.refer_to_underwriters
-     FROM claim 
+          , adjuster_id, syndicate_id, claim.refer_to_underwriters
+     FROM claim
+     LEFT JOIN risk USING (risk_id)
+     LEFT JOIN contract USING (contract_id)
      LEFT JOIN timecard_interim AS ti 
       ON claim.claim_id = ti.claim_id
       WHERE claim.status = 'Open'
-       GROUP BY claim.claim_id 
+       GROUP BY claim.claim_id, syndicate_id
  ), unbilled AS (
        SELECT 'claim'::text AS Heading,
                t.claim_id AS "Claim Number", 
-               sum(minutes) AS "Total Minutes", person_short_name(adjuster_id) AS "Examiner",
+               sum(minutes) AS "Total Minutes",
+               person_short_name(adjuster_id) AS "Examiner",
+               person_name(syndicate_id) AS "Syndicate",
                CASE WHEN refer_to_underwriters THEN 'Yes' ELSE 'No' END AS "Over Authority? (Yes/No)"
 
        FROM timecard AS t RIGHT JOIN interim AS ti 
         ON ti.claim_id = t.claim_id
        WHERE t.date > ti.interim_date 
-       GROUP BY t.claim_id, adjuster_id, refer_to_underwriters
+       GROUP BY t.claim_id, adjuster_id, refer_to_underwriters, syndicate_id
  ), unbilled_gte AS (
    SELECT * from unbilled WHERE "Total Minutes" >= $1
  ), report AS (
@@ -43,7 +47,7 @@ WITH interim AS (
    SELECT 'All Claims'::text,
           count(*),
           sum("Total Minutes"),
-          ' ', NULL
+          ' ', ' ', NULL
      FROM unbilled_gte
 
   ORDER by "Claim Number"
